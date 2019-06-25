82202f318891d5a7cdf914528565b7d16b1e5c47
TAYLOR YAO <yaotaylor25@gmail.com>
2019-06-24 23:01:32 -0400

    file deplyment

---
+++ client/src/Components/Acceuil/Acceuil.js
@@ -0,0 +1,15 @@
+import React, {Component} from "react";
+
+
+class Acceuil extends Component {
+    render(){
+        return(
+        <div style={{width:"100%",margin:"auto"}}>
+            <h1>Acceuil</h1>
+           
+        </div>
+        )
+    }
+}
+
+export default Acceuil;
---
+++ client/src/Components/Commande/Commande.js
@@ -0,0 +1,15 @@
+import React, {Component} from "react";
+
+
+class Commande extends Component {
+    render(){
+        return(
+        <div style={{width:"100%",margin:"auto"}}>
+            <h1>Commande</h1>
+           
+        </div>
+        )
+    }
+}
+
+export default Commande;
